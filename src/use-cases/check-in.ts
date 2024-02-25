eaacbe9dde4b9a53c36c11515be5abd8c1655e4b
Diego Machado <machadodiegoh@gmail.com>
2024-02-25 15:14:14 +0000

    feat: add gym repository and update check-in use case

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,14 +1,29 @@
+import { ResourceNotFoundError } from '@/errors/resource-not-found-error';
 import { CheckInsRepository } from '@/repositories/check-ins-repository';
+import { GymsRepository } from '@/repositories/gyms-repository';
 
 type CheckinParams = {
   userId: string;
   gymId: string;
+  userLatitude: number;
+  userLongitude: number;
 };
 
 export class CheckInUseCase {
-  constructor(private checkInsRepository: CheckInsRepository) {}
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+    private gymsRepository: GymsRepository
+  ) {}
 
-  async execute({ userId, gymId }: CheckinParams) {
+  async execute({ userId, gymId, userLatitude, userLongitude }: CheckinParams) {
+    const gym = await this.gymsRepository.findById(gymId);
+
+    if (!gym) {
+      throw new ResourceNotFoundError();
+    }
+
+    // TODO: calculate distance between user and gym
+
     const hasCheckInOnSameDay =
       await this.checkInsRepository.findByUserIdOnDate(userId, new Date());
 
