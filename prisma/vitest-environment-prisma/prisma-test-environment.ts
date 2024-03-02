6d17e6a964ece941170dd31f127099f16e3aee67
Diego Machado <49965743+diegohmac@users.noreply.github.com>
2024-03-02 17:28:45 +0000

    feat: update setup and teardown methods for prisma-test-environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,40 @@
+import { randomUUID } from 'node:crypto';
+import { execSync } from 'node:child_process';
 import type { Environment } from 'vitest';
+import { PrismaClient } from '@prisma/client';
+
+import 'dotenv/config';
+
+const prisma = new PrismaClient();
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not set');
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set('schema', schema);
+
+  return url.toString();
+}
 
 export default (<Environment>{
   name: 'prisma',
   transformMode: 'ssr',
-  setup() {
-    // custom setup
-    console.log('SETUP');
+  async setup() {
+    const schema = randomUUID();
+    const databaseUrl = generateDatabaseUrl(schema);
+
+    process.env.DATABASE_URL = databaseUrl;
+
+    execSync('npx prisma migrate deploy');
+
     return {
-      teardown() {
-        // called after all tests with this env have been run
-        console.log('TEARDOWN');
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+        await prisma.$disconnect();
       },
     };
   },
