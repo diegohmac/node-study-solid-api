5c4d7b80ab8106fdcb8a83f154ec9a31e9e62823
Diego Machado <49965743+diegohmac@users.noreply.github.com>
2024-03-02 17:39:47 +0000

    test: add authenticate controller e2e

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,6 +1,6 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 import request from 'supertest';
 import { app } from '@/app';
-import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
 describe('Register (e2e)', () => {
   beforeAll(async () => {
