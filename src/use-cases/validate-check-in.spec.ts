b07c2908e279e5fe3ce2a9e60ec54246bd610068
Diego Machado <49965743+diegohmac@users.noreply.github.com>
2024-03-02 13:44:08 +0000

    feat: add validate check in use case

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -1,8 +1,9 @@
-import { expect, describe, it, beforeEach, afterEach } from 'vitest';
+import { expect, describe, it, beforeEach, afterEach, vi } from 'vitest';
 
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-checkins-repository';
 import { ValidateCheckInUseCase } from './validate-check-in';
 import { ResourceNotFoundError } from '@/errors/resource-not-found-error';
+import { LateCheckInValidationError } from '@/errors/late-check-in-validation-error';
 
 let checkInsRepository: InMemoryCheckInsRepository;
 let validateCheckInUseCase: ValidateCheckInUseCase;
@@ -12,11 +13,11 @@ describe('Validate Check In Use Case', () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     validateCheckInUseCase = new ValidateCheckInUseCase(checkInsRepository);
 
-    // vi.useFakeTimers();
+    vi.useFakeTimers();
   });
 
   afterEach(() => {
-    // vi.useRealTimers();
+    vi.useRealTimers();
   });
 
   it('should be able to validate the check-in', async () => {
@@ -41,5 +42,23 @@ describe('Validate Check In Use Case', () => {
         checkInId: 'non-existing-check-in-id',
       })
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it('should not be able to validate the check-in after 20 minutes of creation', async () => {
+    vi.setSystemTime(new Date(2024, 2, 3, 13, 40));
+
+    const createdCheckIn = await checkInsRepository.create({
+      gymId: 'gym-id',
+      userId: 'user-id',
+    });
+
+    const TWENTY_ONE_MINUTES_IN_MS = 21 * 60 * 1000;
+    vi.advanceTimersByTime(TWENTY_ONE_MINUTES_IN_MS);
+
+    await expect(
+      validateCheckInUseCase.execute({
+        checkInId: createdCheckIn.id,
+      })
+    ).rejects.toBeInstanceOf(LateCheckInValidationError);
   });
 });
--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,5 +1,7 @@
+import { LateCheckInValidationError } from '@/errors/late-check-in-validation-error';
 import { ResourceNotFoundError } from '@/errors/resource-not-found-error';
 import { CheckInsRepository } from '@/repositories/check-ins-repository';
+import dayjs from 'dayjs';
 
 type ValidateCheckInParams = {
   checkInId: string;
@@ -13,6 +15,15 @@ export class ValidateCheckInUseCase {
 
     if (!checkIn) {
       throw new ResourceNotFoundError();
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.createdAt,
+      'minutes'
+    );
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError();
     }
 
     checkIn.validatedAt = new Date();
