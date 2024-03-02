874aab49a3b6200f8413c0af55278c2019f9fd5e
Diego Machado <machadodiegoh@gmail.com>
2024-03-02 14:45:09 +0000

    feat: add prisma check-ins repository and update prisma users repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -4,6 +4,14 @@ import { prisma } from '@/lib/prisma';
 import { UsersRepository } from '../user-repository';
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string) {
+    return prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
   async findByEmail(email: string) {
     return prisma.user.findUnique({
       where: {
