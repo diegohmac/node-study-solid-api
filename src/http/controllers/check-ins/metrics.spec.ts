30b30a73de4d3cc4f7156e26df1554718d7f7a2a
Diego Machado <machadodiegoh@gmail.com>
2024-03-03 09:20:40 +0000

    test: add check-ins e2e

---
+++ src/http/controllers/check-ins/metrics.spec.ts
@@ -0,0 +1,50 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import request from 'supertest';
+import { app } from '@/app';
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user';
+import { prisma } from '@/lib/prisma';
+
+describe('Check-in Metrics (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to get the count of check-ins', async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const user = await prisma.user.findFirstOrThrow();
+
+    const gym = await prisma.gym.create({
+      data: {
+        name: 'Typescript Gym',
+        latitude: -23.561399,
+        longitude: -46.655348,
+      },
+    });
+
+    await prisma.checkIn.createMany({
+      data: [
+        {
+          userId: user.id,
+          gymId: gym.id,
+        },
+        {
+          userId: user.id,
+          gymId: gym.id,
+        },
+      ],
+    });
+
+    const response = await request(app.server)
+      .get('/check-ins/metrics')
+      .set('Authorization', `Bearer ${token}`)
+      .send();
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.checkInsCount).toEqual(2);
+  });
+});
